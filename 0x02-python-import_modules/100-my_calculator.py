7ed78a424be671a029a58e0c8f11e02785cfc4e1
Akeh9ja <wilfreddbest1@gmail.com>
2022-06-04 11:19:11 +0000

    taking arguments from command line to make calculations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,22 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    number_args = len(sys.argv)
+    num_args = len(sys.argv)
     if num_args != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     a = int(sys.argv[1])
-    operator = sys.argv[2]
+    op = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator is '+':
-        print('{} {} {} = {}'.format(a, operator, b, add(a, b)))
-    elif operator is '-':
-        print('{} {} {} = {}'.format(a, operator, b, sub(a, b))
-    elif operator is '*':
-        print('{} {} {} = {}'.format(a, operator, b, mul(a, b))
-    elif operator is '/':
-        print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
+    if op is '+':
+        print('{} {} {} = {}'.format(a, op, b, add(a, b)))
+    elif op is '-':
+        print('{} {} {} = {}'.format(a, op, b, sub(a, b)))
+    elif op is '*':
+        print('{} {} {} = {}'.format(a, op, b, mul(a, b)))
+    elif op is '/':
+        print('{} {} {} = {}'.format(a, op, b, div(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
