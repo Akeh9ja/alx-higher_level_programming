0685a6eb0d4f8edbb3d6203b08f5069319d5c611
Akeh9ja <wilfreddbest1@gmail.com>
2022-07-12 21:16:18 +0000

    added all files for almost a circle task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""Defines a JSON file-writing function."""
+"""Module 7-save_to_json_file.
+   Writes an object to a text file,
+   Using a JSON representation.
+"""
+
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write an object to text file using JSON representation"""
+    """Writes the representation of my_obj
+    to fiename.
+
+    Args:
+        - my_obj: object to write
+        - filename: file to write into
+    """
+
     with open(filename, "w+") as f:
         json.dumps(my_obj, f)
