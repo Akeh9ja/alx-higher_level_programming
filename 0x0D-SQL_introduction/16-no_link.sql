ea6c7bcbb5a4fa6b72980036c912c3d4c803d42a
Akeh9ja <wilfreddbest1@gmail.com>
2022-08-10 11:32:07 +0100

    changed syntax

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,2 @@
 -- Write a script that lists all records of the table second_table of the database 
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
--- Insert new values
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (6, 'Aria', 18);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (7, 'Aria', 12);
--- listing rows and column
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT `score`, `name` FROM second_table WHERE `name` IS NOT NULL ORDER BY `score` DESC;
