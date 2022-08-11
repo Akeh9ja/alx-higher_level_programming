70cf3d8e5ecdc26da4e45dd776b1d3ee9504beb5
Akeh9ja <wilfreddbest1@gmail.com>
2022-08-12 00:39:08 +0100

    refrenced the database in the state

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,4 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Use the database
-USE hbtn_0d_usa;
 -- create table
-CREATE TAABLE IF NOT EXISTS states (id INT Unique NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TAABLE IF NOT EXISTS hbtn_0d_usa.states (id INT Unique NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
